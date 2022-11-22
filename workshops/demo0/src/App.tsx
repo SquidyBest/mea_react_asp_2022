74daf1a697271d9c2720579f80926d8425dba225
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-22 09:34:21 +0700

    1234

--- workshops/demo0/src/App.tsx
+++ workshops/demo0/src/App.tsx
@@ -3,14 +3,20 @@ import React from "react";
 type Props = {};
 
 export default function App({}: Props) {
-  const tmp1 = 1234;
+  let tmp1 = 1234;
 
   return (
     <div>
       <h1>App</h1>
       <span>MEA {tmp1}</span>
       <br />
-      <button onClick={() => alert("Hey")}>Click</button>
+      <button
+        onClick={() => {
+          tmp1 = tmp1 + 1;
+        }}
+      >
+        Click
+      </button>
     </div>
   );
 }
