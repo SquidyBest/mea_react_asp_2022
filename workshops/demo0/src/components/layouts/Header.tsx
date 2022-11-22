11e822bd1117cd4b053ef876516fece3471a55c4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-22 10:52:21 +0700

    1234

--- workshops/demo0/src/components/layouts/Header.tsx
+++ workshops/demo0/src/components/layouts/Header.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 type Props = {
   title: string;
+  onClick: () => void;
 };
 
 export default function Header(props: Props) {
-  return <div>Header: {props.title}</div>;
+  return (
+    <div>
+      Header: {props.title} :
+      <button onClick={() => props.onClick()}>Click Me</button>
+    </div>
+  );
 }
