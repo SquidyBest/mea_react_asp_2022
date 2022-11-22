4401f743ae81d471a2abb49f9316308431bc8163
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-11-22 09:42:52 +0700

    1234

--- workshops/demo0/src/App.tsx
+++ workshops/demo0/src/App.tsx
@@ -4,18 +4,30 @@ type Props = {};
 
 export default function App({}: Props) {
   let tmp1 = 1234;
+  const [tmp2, setTmp2] = React.useState(0);
 
   return (
     <div>
       <h1>App</h1>
-      <span>MEA {tmp1}</span>
+      <span>
+        MEA {tmp1}:{tmp2}
+      </span>
       <br />
       <button
         onClick={() => {
           tmp1 = tmp1 + 1;
+          console.log(`Tmp1: ${tmp1}`);
         }}
       >
-        Click
+        Click1
+      </button>
+
+      <button
+        onClick={() => {
+          setTmp2(tmp2 + 1);
+        }}
+      >
+        Click2
       </button>
     </div>
   );
